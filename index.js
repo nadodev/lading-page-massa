5159ea96de81c797201b92f93bee4e23ff1e8bc6
Leonardo Geja <leonardo.geja@dotgroup.com.br>
2022-05-16 18:34:58 -0300

    fix: ajuste para deixar html mais semantico e adicionado login

--- index.js
+++ index.js
@@ -27,3 +27,88 @@ const goToTop = () => {
 };
 
 btnTop.addEventListener("click", goToTop);
+
+// function login
+const form = document.querySelector(".signin");
+const loginBox = document.querySelector(".login");
+const email = document.querySelector(".email");
+const senha = document.querySelector(".senha");
+const btnLogin = document.querySelector(".btn-entrar");
+const btnSair = document.querySelector(".btn-sair");
+const modal = document.querySelector(".modal");
+const modalSair = document.querySelector(".modal-sair");
+const overlay = document.querySelector(".overlay");
+const btnFecharModal = document.querySelector(".close");
+const btnFecharModalSair = document.querySelector(".close-sair");
+const user = document.querySelector(".user");
+
+function handleSubmit(e) {
+  e.preventDefault();
+  login();
+}
+
+function login() {
+  if (email.value === "" || senha.value === "") {
+    alert("Preencha todos os campos");
+
+    return;
+  }
+
+  let textEmail = email.value;
+  let textSenha = senha.value;
+
+  if (textEmail !== null && !textSenha !== null) {
+    openModal();
+    const storage = localStorage.setItem("email", textEmail);
+    console.log(storage);
+  }
+}
+
+function verificaLogin() {
+  const storage = localStorage.getItem("email");
+
+  if (storage !== null) {
+    form.style.display = "none";
+    loginBox.style.display = "flex";
+    user.innerHTML = storage;
+  }
+}
+verificaLogin();
+
+function signin() {
+  storage = localStorage.getItem("email");
+
+  if (storage !== null) {
+    openSair();
+  }
+}
+
+btnSair.addEventListener("click", signin);
+
+function openModal() {
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal() {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+  window.location.reload();
+}
+
+function openSair() {
+  modalSair.classList.add("active");
+  overlay.classList.add("active");
+  email.value = "";
+  senha.value = "";
+}
+
+function closeSair() {
+  modalSair.classList.remove("active");
+  overlay.classList.remove("active");
+  localStorage.removeItem("email");
+  window.location.reload();
+}
+
+btnFecharModal.addEventListener("click", closeModal);
+btnFecharModalSair.addEventListener("click", closeSair);
